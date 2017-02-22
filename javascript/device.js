e8f8040b75c7b5555c680bedce6b46daaa45ada8
skittishtrigger <skittishtrigger@users.noreply.github.com>
2017-02-22 00:23:58 -0500

    fix?
    Figured it out maybe?  .setattribute!

--- javascript/device.js
+++ javascript/device.js
@@ -16,16 +16,16 @@ function adjustStyle() {
       w = 900;
       confirm("Your screen does not respond with its size");
     } else if (w < 360) {
-      document.getElementById("size-stylesheet").setAttribute("href", "css/small");
+      document.getElementById("size-stylesheet").setAttribute("href", "css/small.css");
     } else if (w < 480) {
        document.getElementById("size-stylesheet").setAttribute("href", "css/medium.css");
     } else if (w < 720) {
-      document.getElementById("size-stylesheet").attr("href", "css/large.css");
+      document.getElementById("size-stylesheet").setAttribute("href", "css/large.css");
     } else if (w > 721) {
-       document.getElementById("size-stylesheet").attr("href", "css/default.css");
+       document.getElementById("size-stylesheet").setAttribute("href", "css/default.css");
       confirm("WoW, that " + w + "px screen is huge");
     } else {
-     document.getElementById("#size-stylesheet").attr("href", "css/default.css");
+     document.getElementById("#size-stylesheet").setAttribute("href", "css/default.css");
     }
     // if/else if statements  ^^^^  "linking" proper css based on screen.width
   }
